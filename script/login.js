dc7219834a667f4326d264beea6d84290b511d13
404Shihab <shihabbhuiyan500@gmail.com>
2026-02-23 20:10:37 +0600

    login page complete and home page added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,26 @@
-console.log('login function');
+// console.log('login function');
+document.getElementById('btn-login').addEventListener('click',function()
+{
+    // 1. Get the mobile number input
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    // 2. Get the pin input
+    const pinInput= document.getElementById('input-pin');
+    const pinNumber = pinInput.value;
+    console.log(pinNumber);
+    // 3. Match mobile number and pin
+    if(contactNumber == "01984910007" && pinNumber == "5798")
+        {// 3- true: homepage
+        alert('Login Success!!')
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+        }
+    
+    // 3- false: return
+    else{
+        alert('Login Failed');
+        return; 
+    }
+        
+})
